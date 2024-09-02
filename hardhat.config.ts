f7a835604f97eeb57b7e81e206fec95b0602d518
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-09-02 12:00:48 -0300

    first commi

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.25",
+  defaultNetwork: "hardhat",
+  networks: {
+    avalancheFuji: { 
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: []
+    },
+    avalancheMainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      chainId: 43114,
+      accounts: []
+     },
+  }
+};
+
+export default config;
