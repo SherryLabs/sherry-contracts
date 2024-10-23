2883bfeba7fcf217d70e76572189eb53407c1b62
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-10-23 16:44:02 -0300

    implementing wormhole example sender and receiver

---
+++ contracts/wormhole/SL1MessageSender.sol
@@ -0,0 +1,40 @@
+// SPDX-license-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "wormhole-solidity-sdk/interfaces/IWormholeRelayer.sol";
+
+contract SL1MessageSender {
+    IWormholeRelayer public s_wormholeRelayer;
+    uint256 constant GAS_LIMIT = 50_000;
+
+    constructor(address _wormholeRelayer) {
+        s_wormholeRelayer = IWormholeRelayer(_wormholeRelayer);
+    }
+
+    function quoteCrossChainCost(
+        uint16 _targetChain
+    ) public view returns (uint256 cost) {
+        (cost, ) = s_wormholeRelayer.quoteEVMDeliveryPrice(
+            _targetChain,
+            0,
+            GAS_LIMIT
+        );
+    }
+
+    function sendMessage(
+        uint16 _targetChain,
+        address _targetAddress,
+        string calldata _message
+        //bytes memory _payload,
+        //uint256 _gasLimit
+    ) external payable {
+        uint256 cost = quoteCrossChainCost(_targetChain);
+        s_wormholeRelayer.sendPayloadToEvm{value: cost}(
+            _targetChain,
+            _targetAddress,
+            abi.encode(_message, msg.sender),
+            0,
+            GAS_LIMIT
+        );
+    }
+}
